1ef9677cdd90043f66956565594e417f41973e07
Guilherme Frota <guiquintelas96@gmail.com>
2020-03-23 20:42:00 -0300

    add animation on page change

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,9 @@
     </v-app-bar>
 
     <v-content>
-      <router-view></router-view>
+      <transition name="slide-fade" mode="out-in">
+        <router-view></router-view>
+      </transition>
     </v-content>
   </v-app>
 </template>
@@ -47,3 +49,18 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.slide-fade-enter-active {
+  transition: all .15s ease;
+}
+
+.slide-fade-leave-active {
+  transition: all 0s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateY(3px);
+  opacity: 0;
+}
+</style>
