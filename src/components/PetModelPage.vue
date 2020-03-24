d6e627338994fe3d74018d008247de029ca6f8ad
Guilherme Frota <guiquintelas96@gmail.com>
2020-03-24 19:21:32 -0300

    implement add user feature

--- src/components/PetModelPage.vue
+++ src/components/PetModelPage.vue
@@ -1,25 +1,71 @@
 <template>
   <PetPage :title="title">
     <v-row>
-      <v-col v-if="type == 'list'">
+      <v-col>
         <v-btn
+          v-if="type == 'list'"
           exact
           color="primary"
-          to="create">
+          :to="`${module}/create`">
           {{ newModelBtnText }}
+        </v-btn>
+
+        <v-btn
+          v-else
+          exact
+          :to="`/${module}`">
+          GO BACK
         </v-btn>
       </v-col>
     </v-row>
 
-    <v-row v-if="type == 'list'">
-      <v-col>
-        <v-data-table
-          :headers="headers"
-          :items="$store.direct.state[module].models"
-          class="elevation-1"
-        ></v-data-table>
-      </v-col>
-    </v-row>
+    <transition name="slide-fade" mode="out-in">
+      <v-row v-if="type == 'list'" key="list">
+        <v-col>
+          <v-data-table
+            :headers="headers"
+            :items="$store.direct.state[module].models"
+            class="elevation-1"
+          ></v-data-table>
+        </v-col>
+      </v-row>
+
+      <v-row v-if="type == 'create'" key="create">
+        <v-col>
+          <v-card>
+            <v-card-title>
+              <h3>Creating new <span class="text-capitalize">{{ module }}</span></h3>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-form ref="form">
+                  <slot :model="model"></slot>
+                </v-form>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn
+                text
+                exact
+                :to="`/${module}`">
+                CANCEL
+              </v-btn>
+
+              <v-btn
+                text
+                color="primary"
+                @click="submit">
+                {{ type == 'create' ? 'CREATE' : 'UPDATE' }}
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </transition>
   </PetPage>
 </template>
 
@@ -42,6 +88,37 @@ export default Vue.extend({
     type: String as () => PageType,
     module: String,
     headers: Array as () => DataTableHeader[],
+    initModel: Function,
+  },
+
+  data: () => ({
+    model: {},
+  }),
+
+  created() {
+    if (this.initModel) {
+      this.model = this.initModel();
+    }
+  },
+
+  watch: {
+    type() {
+      if (this.initModel) {
+        this.model = this.initModel();
+      }
+    },
+  },
+
+  methods: {
+    submit() {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      if (!(this.$refs.form as any).validate()) {
+        return;
+      }
+
+      this.$emit('create', this.model);
+      this.$router.push(`/${this.module}`);
+    },
   },
 });
 </script>
--- src/views/User.vue
+++ src/views/User.vue
@@ -5,7 +5,49 @@
     module="user"
     :headers="headers"
     :type="type"
-  ></PetModelPage>
+    :initModel="initModel"
+    @create="onCreate"
+  >
+    <template v-slot="{ model }">
+      <v-row>
+        <v-col>
+          <v-text-field
+            label="ID"
+            outlined
+            v-model="model.id"
+            readonly
+          ></v-text-field>
+        </v-col>
+
+        <v-col>
+          <v-text-field
+            label="Name"
+            outlined
+            v-model="model.name"
+            :rules="[validations.required]"
+          ></v-text-field>
+        </v-col>
+
+        <v-col>
+          <v-text-field
+            label="Email"
+            outlined
+            v-model="model.email"
+            :rules="[validations.required, validations.email]"
+          ></v-text-field>
+        </v-col>
+
+        <v-col>
+          <v-text-field
+            label="Password"
+            outlined
+            v-model="model.password"
+            :rules="[validations.required]"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </template>
+  </PetModelPage>
 </template>
 
 
@@ -13,6 +55,9 @@
 import Vue from 'vue';
 import PetModelPage from '@/components/PetModelPage.vue';
 import { DataTableHeader } from 'vuetify';
+import { useValidations } from '@/hooks/validations';
+import store from '@/store/store';
+import { User } from '../store/user.module';
 
 export default Vue.extend({
   name: 'User',
@@ -38,7 +83,15 @@ export default Vue.extend({
   }),
 
   methods: {
-    //
+    initModel() {
+      return {
+        id: Math.random().toString(36).substring(3, 8),
+      };
+    },
+
+    onCreate(model: User) {
+      store.commit.user.ADD_MODEL(model);
+    },
   },
 });
 </script>
