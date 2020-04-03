6ab6010a8464686fce4ee9c32b54797cc87cc3b4
Guilherme Frota <guiquintelas96@gmail.com>
2020-04-03 18:42:58 -0300

    add PetLink component

---
+++ src/components/PetLink.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <router-link style="text-decoration: none" :to="{ path: `${module}/${id}/update` }">
+    <slot v-bind="model">
+      {{ model.name }}
+    </slot>
+  </router-link>
+</template>
+
+<script lang="ts">
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import Vue from 'vue';
+import store from '@/store/store';
+
+export default Vue.extend({
+  name: 'PetLink',
+
+  props: {
+    module: String,
+    id: String,
+  },
+
+  computed: {
+    model(): any {
+      return store.getters[this.module].filterById(this.id);
+    },
+  },
+});
+</script>
