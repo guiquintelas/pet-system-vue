c313206f95cb7b964dcafd671c8b690237c75a88
Guilherme Frota <guiquintelas96@gmail.com>
2020-04-06 20:08:38 -0300

    add vaccines notifications

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,38 @@
 <template>
-  <PetPage title="Home"></PetPage>
+  <PetPage title="Home">
+    <v-row>
+      <v-col
+        v-for="vaccine in $store.direct.getters.vaccine.withNotification"
+        :key="vaccine.id"
+        cols="12"
+        sm="6">
+        <v-alert
+          type="info">
+          The vaccine
+          <router-link
+            class="notification-link"
+            :to="`vaccine/${vaccine.id}/update`">
+            {{ vaccine.name }}
+          </router-link>
+          is now available for ${{ vaccine.price | currency }}
+        </v-alert>
+      </v-col>
+
+      <v-col
+        v-if="$store.direct.getters.vaccine.withNotification.length === 0"
+        class="subtitle-1 font-weight-light ml-2 grey--text align-center"
+        style="display: flex">
+        <v-icon color="grey" class="mr-2">mdi-bell-outline</v-icon>
+        No new notifications
+      </v-col>
+    </v-row>
+  </PetPage>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import PetPage from '@/components/PetPage.vue';
+import { useFilters } from '../hooks';
 
 export default Vue.extend({
   name: 'Home',
@@ -12,5 +40,17 @@ export default Vue.extend({
   components: {
     PetPage,
   },
+
+  filters: {
+    ...useFilters(),
+  },
 });
 </script>
+
+<style lang="scss" scoped>
+
+.notification-link {
+  color: white;
+}
+
+</style>
