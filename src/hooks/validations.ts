82843798f5b58c277eafdaa13b868b6974cb58ed
Guilherme Frota <guiquintelas96@gmail.com>
2020-04-06 14:00:39 -0300

    translate error msgs

--- src/hooks/validations.ts
+++ src/hooks/validations.ts
@@ -24,6 +24,7 @@ export function useValidations() {
         if (!cpf) return false;
 
         cpf = cpf.replace(/[^\d]+/g, '');
+
         if (cpf === '') return 'Invalid CPF!';
 
         if (cpf.length !== 11) {
@@ -40,20 +41,20 @@ export function useValidations() {
             || cpf === '66666666666'
             || cpf === '77777777777'
             || cpf === '88888888888'
-            || cpf === '99999999999') { return 'Cpf inválido'; }
+            || cpf === '99999999999') { return 'Invalid CPF!'; }
         // Valida 1o digito
         let add = 0;
         let rev = 0;
         for (let i = 0; i < 9; i++) { add += parseInt(cpf.charAt(i)) * (10 - i); }
         rev = 11 - (add % 11);
         if (rev === 10 || rev === 11) { rev = 0; }
-        if (rev !== parseInt(cpf.charAt(9))) { return 'Cpf inválido'; }
+        if (rev !== parseInt(cpf.charAt(9))) { return 'Invalid CPF!'; }
         // Valida 2o digito
         add = 0;
         for (let i = 0; i < 10; i++) { add += parseInt(cpf.charAt(i)) * (11 - i); }
         rev = 11 - (add % 11);
         if (rev === 10 || rev === 11) { rev = 0; }
-        if (rev !== parseInt(cpf.charAt(10))) { return 'Cpf inválido'; }
+        if (rev !== parseInt(cpf.charAt(10))) { return 'Invalid CPF!'; }
         return true;
       },
     },
