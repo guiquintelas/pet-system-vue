ff02e36db7ba3de36ea55517a1c87e78ea73b7bc
Guilherme Frota <guiquintelas96@gmail.com>
2020-04-06 20:07:45 -0300

    fix cpf filter

--- src/hooks/filters.ts
+++ src/hooks/filters.ts
@@ -5,11 +5,15 @@ export function useFilters() {
       return `${dia}/${mes}/${ano}`;
     },
 
-    cpf(cpf: string) {
+    cpf(cpf: string | undefined) {
+      if (!cpf) {
+        return '';
+      }
+
       const cpf1 = cpf.substr(0, 3);
-      const cpf2 = cpf.substr(2, 3);
-      const cpf3 = cpf.substr(4, 3);
-      const cpf4 = cpf.substr(6, 2);
+      const cpf2 = cpf.substr(3, 3);
+      const cpf3 = cpf.substr(6, 3);
+      const cpf4 = cpf.substr(9, 2);
 
       return `${cpf1}.${cpf2}.${cpf3}-${cpf4}`;
     },
