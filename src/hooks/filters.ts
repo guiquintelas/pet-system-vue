6be50a2b2b674824cb154673f4f3356e17bdd875
Guilherme Frota <guiquintelas96@gmail.com>
2020-04-06 12:40:27 -0300

    add cpf filter and validation

--- src/hooks/filters.ts
+++ src/hooks/filters.ts
@@ -4,5 +4,14 @@ export function useFilters() {
       const [ano, mes, dia] = date.split('-');
       return `${dia}/${mes}/${ano}`;
     },
+
+    cpf(cpf: string) {
+      const cpf1 = cpf.substr(0, 3);
+      const cpf2 = cpf.substr(2, 3);
+      const cpf3 = cpf.substr(4, 3);
+      const cpf4 = cpf.substr(6, 2);
+
+      return `${cpf1}.${cpf2}.${cpf3}-${cpf4}`;
+    },
   };
 }
--- src/hooks/validations.ts
+++ src/hooks/validations.ts
@@ -1,3 +1,5 @@
+/* eslint-disable radix */
+/* eslint-disable no-plusplus */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export function useValidations() {
@@ -16,6 +18,43 @@ export function useValidations() {
 
         return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v)
           || 'Invalid email!';
+      },
+
+      cpf(cpf: string) {
+        if (!cpf) return false;
+
+        cpf = cpf.replace(/[^\d]+/g, '');
+        if (cpf === '') return 'Invalid CPF!';
+
+        if (cpf.length !== 11) {
+          return 'Invalid CPF!';
+        }
+
+        // Elimina CPFs invalidos conhecidos
+        if (cpf === '00000000000'
+            || cpf === '11111111111'
+            || cpf === '22222222222'
+            || cpf === '33333333333'
+            || cpf === '44444444444'
+            || cpf === '55555555555'
+            || cpf === '66666666666'
+            || cpf === '77777777777'
+            || cpf === '88888888888'
+            || cpf === '99999999999') { return 'Cpf inválido'; }
+        // Valida 1o digito
+        let add = 0;
+        let rev = 0;
+        for (let i = 0; i < 9; i++) { add += parseInt(cpf.charAt(i)) * (10 - i); }
+        rev = 11 - (add % 11);
+        if (rev === 10 || rev === 11) { rev = 0; }
+        if (rev !== parseInt(cpf.charAt(9))) { return 'Cpf inválido'; }
+        // Valida 2o digito
+        add = 0;
+        for (let i = 0; i < 10; i++) { add += parseInt(cpf.charAt(i)) * (11 - i); }
+        rev = 11 - (add % 11);
+        if (rev === 10 || rev === 11) { rev = 0; }
+        if (rev !== parseInt(cpf.charAt(10))) { return 'Cpf inválido'; }
+        return true;
       },
     },
   };
