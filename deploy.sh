6aa2d68dae9596edc0476d03a6a843006e12e2fe
Guilherme Frota <guiquintelas96@gmail.com>
2020-05-28 10:47:15 -0300

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:guiquintelas/pet-system-vue.git master:gh-pages
+
+cd -
